173ce83ea9b7436815ab5d95402226c9bbfc10a9
Amey Mahajan <amey.mahajan@metricstream.com>
2021-02-06 20:25:34 +0530

    converted routes to class component

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/home.js';
 
-const Routes = () => {
-  // const print = () => {
-  //   return "This is first route";
-  // }
-
-  return (
-    <div>
-      <Switch>
-        <Route path="/home" exact component = {Home}/>
-      </Switch>
+class Routes extends Component {
+  render() {
+    return (
       <div>
-        <h2>Vaccine is out from Russia.</h2>
-        <Home/>
+        <Switch>
+          <Route path="/home" exact component = {Home}/>
+        </Switch>
+        <div>
+          <h2>Vaccine is out from Russia.</h2>
+          <Home/>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
 export default Routes;
