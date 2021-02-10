91cb65eea4a6a0abc061222e1bd7ea291224e60b
Amey Mahajan <amey.mahajan@metricstream.com>
2021-02-11 00:54:17 +0530

    understood basic hoc concept, created layout_hoc

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import Home from './components/Home/home.js';
+import Layout from './hoc/Layout/layout.js';
 
 class Routes extends Component {
   render() {
     return (
-      <div>
-        <Switch>
-          <Route path="/home" exact component = {Home}/>
-        </Switch>
+      <Layout>
         <div>
-          <h2>Vaccine is out from Russia.</h2>
-          <Home/>
+          <Switch>
+            <Route path="/home" exact component = {Home}/>
+          </Switch>
+          <div>
+            <h2>Vaccine is out from Russia.</h2>
+            <Home/>
+          </div>
         </div>
-      </div>
+      </Layout>
     )
   }
 }
